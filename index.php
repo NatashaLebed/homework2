1683ca1d2e8dc545a56204d93daa328ca23694a9
NatashaLebed <Natasha.lebed@gmail.com>
2013-10-23 13:40:22 +0300

    implementation of interfaces

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+
 interface FoldInterface
 {
-    public function doFold();
+    public  function Fold();
 }
 
 interface ProtectColdInterface
@@ -40,34 +41,69 @@ abstract class AbstractPerambulator
     }
 }
 
-class SummerPerambulator extends AbstractPerambulator
+class SummerPerambulator extends AbstractPerambulator implements FoldInterface
 {
     public function __construct($brand, $weight, $numWheels)
     {
         $this->Create($brand, $weight, $numWheels);
     }
+
     public function PriceTitle()
     {
         echo ("SummerPeramb - $this->brand, $this->weight, $this->numWheels");
     }
 
+    public function Fold()
+    {
+       $operations = 3;
+        echo "Тип сложения - трость. Сложилась за $operations действия ";
+    }
+
+
+
+
 }
 
-class TransformerPerambulator extends AbstractPerambulator
+class TransformerPerambulator extends AbstractPerambulator implements FoldInterface, ProtectColdInterface, ReversibleHandleInterface
 {
     public function __construct($brand, $weight, $numWheels)
     {
         $this->Create($brand, $weight, $numWheels);
     }
+
     public function PriceTitle()
     {
         echo ("Transformer - $this->brand, $this->weight, $this->numWheels");
     }
 
+    public function Fold()
+    {
+        $operations = 5;
+        echo "Тип сложения - книжка. Сложилась за $operations действий ";
+    }
+
+    public function ProtectCold()
+    {
+        echo 'Чехол - есть, Плотная ткань - есть';
+    }
+
+    public function ReversibleHandle()
+    {
+        echo 'Можно перкинуть ручку';
+    }
+
+
 }
 
 $MySummer = new SummerPerambulator('Chikko','7','3');
+echo '<br>';
 $MySummer->PriceTitle();
+echo '<br>';
+$MySummer->Fold();
+echo '<br>';
 $MyTransformer = new TransformerPerambulator('GoodBaby','15','4');
-$MySummer->PriceTitle();
+echo '<br>';
+$MyTransformer->PriceTitle();
+echo '<br>';
+$MyTransformer->Fold();
 ?>
